6c54c5b864ef6e0d72362a1b0b7dc330726f055e
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-11-05 16:30:00 -0700

    Add flip card functionality to html using js

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ var welcomeSection = document.querySelector(".welcome-instructions");
 var emptyInputMessage = document.querySelector(".empty-input-message");
 var instructionsSectionParent = document.querySelector(".welcome-instructions");
 var winnerScreenSectionParent = document.querySelector(".winner-screen");
+var gameBoardSectionParent = document.querySelector(".card-table");
 
 formParent.addEventListener('click', onFormParentClick);
+instructionsSectionParent.addEventListener('click', onInstructionsSectionParentClick);
+gameBoardSectionParent.addEventListener('click', flipCard);
 playerOneInput.addEventListener('keyup', onInputEntry);
 playerTwoInput.addEventListener('keyup', onInputEntry);
-instructionsSectionParent.addEventListener('click', onInstructionsSectionParentClick);
-
 
 function onFormParentClick() {
   if (event.target.classList.contains("enable-play-game-btn")) {
@@ -26,8 +27,13 @@ function onInstructionsSectionParentClick() {
   console.log(event.target.classList.contains("welcome-btn"));
   if (event.target.classList.contains("welcome-btn")) {
     welcomeSection.style.display = "none";
-    winnerScreenSectionParent.style.display = "flex";
+    setupGame();
+    gameBoardSectionParent.style.display = "flex";
   }
+}
+
+function setupGame() {
+
 }
 
 function onInputEntry() {
@@ -42,8 +48,22 @@ function onInputEntry() {
 function storePlayerNames() {
   localStorage.setItem("playerOneName", playerOneInput.value);
   localStorage.setItem("playerTwoName", playerTwoInput.value);
-  document.getElementById("player-one-name").innerText = playerOneInput.value;
-  document.getElementById("player-two-name").innerText = playerTwoInput.value;
+  setPlayerNames();
   playerInputForm.style.display = "none";
   welcomeSection.style.display = "flex";
+}
+
+function setPlayerNames() {
+  document.getElementById("player-one-name").innerText = playerOneInput.value;
+  document.getElementById("player-two-name").innerText = playerTwoInput.value;
+  document.querySelector(".match-player-one-name").innerText = playerOneInput.value;
+  document.querySelector(".match-player-two-name").innerText = playerTwoInput.value;
+}
+
+function flipCard() {
+  if (event.target.classList.contains("card__face--front")) {
+    var card = event.target.closest(".card");
+    card.classList.toggle('is-flipped');
+    card.style.background = "none";
+  }
 }
